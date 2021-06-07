15d120bd9f62d79558a49650bb80faf3c246c62c
machida <machida@kmc.gr.jp>
2021-06-08 03:19:24 +0900

    add EQ, NOT_EQ

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -22,7 +22,11 @@ func TestNextToken(t *testing.T) {
     return true;
   } else {
     return false;
-  }`
+  }
+
+  10 == 10;
+  10 != 9;
+  `
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -75,23 +79,31 @@ func TestNextToken(t *testing.T) {
 		{token.GT, ">"},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
-    {token.IF, "if"},
-    {token.LPAREN, "("},
-    {token.INT, "5"},
-    {token.LT, "<"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
     {token.INT, "10"},
-    {token.RPAREN, ")"},
-    {token.LBRACE, "{"},
-    {token.RETURN, "return"},
-    {token.TRUE, "true"},
+    {token.EQ, "=="},
+    {token.INT, "10"},
     {token.SEMICOLON, ";"},
-    {token.RBRACE, "}"},
-    {token.ELSE, "else"},
-    {token.LBRACE, "{"},
-    {token.RETURN, "return"},
-    {token.FALSE, "false"},
+    {token.INT, "10"},
+    {token.NOT_EQ, "!="},
+    {token.INT, "9"},
     {token.SEMICOLON, ";"},
-    {token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
 
--- token/token.go
+++ token/token.go
@@ -25,6 +25,9 @@ const (
 	LT = "<"
 	GT = ">"
 
+	EQ     = "=="
+	NOT_EQ = "!="
+
 	// デリミタ
 	COMMA     = ","
 	SEMICOLON = ";"

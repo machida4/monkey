b99020a4ad30765815c559882cf83471e3b5499a
machida <machida@kmc.gr.jp>
2021-06-11 02:46:49 +0900

    add parser for prefix expression

--- parser/parser.go
+++ parser/parser.go
@@ -42,6 +42,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	// p.infixParseFns = make(map[token.TokenType]infixParseFn)
 
 	// 2つトークンを読み込みcurToken, peekTokenの両方をセット
@@ -50,6 +52,11 @@ func New(l *lexer.Lexer) *Parser {
 
 	return p
 }
+
+type (
+	prefixParseFn func() ast.Expression               // 前置構文解析関数
+	infixParseFn  func(ast.Expression) ast.Expression // 中置構文解析関数
+)
 
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
@@ -156,6 +163,7 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	// prefixを実行してast.Expressionを得てleftExpに格納
@@ -182,17 +190,29 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 	return lit
 }
 
-type (
-	prefixParseFn func() ast.Expression               // 前置構文解析関数
-	infixParseFn  func(ast.Expression) ast.Expression // 中置構文解析関数
-)
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
 
+	p.nextToken()
 
-// Utilities
+	expression.Right = p.parseExpression(PREFIX)
+
+	return expression
+}
+
+// Helpers
 func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
 	p.prefixParseFns[tokenType] = fn
 }
 
 func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
 	p.infixParseFns[tokenType] = fn
+}
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
 }
